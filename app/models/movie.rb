2534a1b1f441b47d9c17ec65d027fe174c46fde3
Tim <g00333333@gmit.ie>
2019-02-21 19:30:17 +0000

    Added enum method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+    # creating enumerable collection of all possible ratings
+    def self.all_ratings
+      result = {}
+      
+      # selecting each unique rating
+  	  self.select(:rating).uniq.each do |movie|
+  		result[movie.rating] = 1
+	  end
+	  
+  	  return result
+    end
 end
